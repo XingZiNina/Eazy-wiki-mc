836dd1c4c572cbad8e3092dc5820831ada259a00
XingZiNina <3435437646@qq.com>
2025-07-24 15:18:55 +0800

    11111

--- docs/components.d.ts
+++
@@ -1,8 +0,0 @@
-export {}
-
-declare module 'vue' {
-  export interface GlobalComponents {
-    RainbowAnimationSwitcher: typeof import('./.vitepress/theme/components/RainbowAnimationSwitcher.vue')['default']
-    RainbowSwitcher: typeof import('./.vitepress/theme/components/RainbowSwitcher.vue')['default']
-  }
-}
